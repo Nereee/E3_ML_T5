6a7686e8269b183723e5acd0a603762f085be890
in1dm3-d <mirari.casillasja@elorreita-errekamari.com>
2024-02-05 11:40:22 +0100

    lehen sprint1 amaituta

--- erronka3/filmak/js/filmak.js
+++ erronka3/filmak/js/filmak.js
@@ -1,5 +1,5 @@
 sarrerakerosiButton.onclick = function (e) {
     e.preventDefault();
 
-    window.location.replace('../../sarrerakerosi/sarrerakerosi.html');
+    window.location.replace('../../sarrerakerosi/sarrerakerosi.php');
 };
