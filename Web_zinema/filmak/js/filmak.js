6ddbf55dc3c132b4d4a71e161d372a22dc39a8cf
in1dm3-d <mirari.casillasja@elorreita-errekamari.com>
2024-02-08 11:41:11 +0100

    php saiaketa

--- Web_zinema/filmak/js/filmak.js
+++ Web_zinema/filmak/js/filmak.js
@@ -1,26 +1,10 @@
-    sarrerakerosiButton.onclick = function (e) {
-    e.preventDefault();
 
-    window.location.replace('../../sarrerakerosi/sarrerakerosi.php');
-
-
-};
-function lehioa() {
-    let filma = document.getElementById("id_film").value
-
-    window.location.replace('../../sarrerakerosi/sarrerakerosi.php?film=' + filma);
-
-
-}
-
-function datuakkargatu() {
-    let filmid = window.location.href.split("=")[1]
-    window.alert(filmid);
-
-    //  0<? php
-    // $filmid = $_POST['filmid'];
-    // echo "La variable filmid es: ".$filmid;
-    //  $sql = "Select izena from filma where id_filma = filmid";
-    //  $mysqli -> query($sql)
-   
-}
+    
+    function lehioa() {
+        let filma = document.getElementById("id_film").value
+    
+        window.location.replace('../../sarrerakerosi/sarrerakerosi.php?film=' + filma);
+    
+    
+    }
+    
--- Web_zinema/sarrerakerosi/sarrerakerosi.php
+++ Web_zinema/sarrerakerosi/sarrerakerosi.php
@@ -12,6 +12,9 @@ if ($mysqli->connect_error) {
     die("Connection failed: " . $mysqli->connect_error);
   }  
 
+  // $sql = "Select izena from filma where filma_id = filmid";
+  // $mysqli -> query($sql)
+
 // Konexioa itxi
 //$mysqli->close();
 
@@ -67,6 +70,42 @@ if ($mysqli->connect_error) {
         <button id="sarrerakerosiButton">Sarrerak erosi</button>
     </form>
 
-    <script src="../filmak/js/filmak.js"></script>
+
+<script>
+
+const zinema = document.getElementById('zinemak');
+const ordua = document.getElementById('saioak');
+const data = document.getElementById('data');
+const prezioa = document.getElementById('prezioa');
+const kant = document.getElementById('kant');
+const sarrerakerosiButton = document.getElementById('sarrerakerosiButton');
+
+sarrerakerosiButton.onclick = function (e) {
+    e.preventDefault();
+
+    const data = {
+        zinema: zinema.value,
+        ordua: ordua.value,
+        prezioa: prezioa.value,
+        kant: kant.value,
+    };
+
+    console.log(data);
+    window.location.replace('../logina/logina.php');
+};
+
+
+function datuakkargatu() {
+    
+
+
+    let filmid = window.location.href.split("=")[1]
+    console.log(filmid)
+
+    
+   
+}
+
+    </script>
 </body>
 </html>
