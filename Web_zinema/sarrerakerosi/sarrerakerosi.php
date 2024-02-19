9f5e52d6f3d9c252325ea0be180b8af7e8e2dd3e
jonnaan <jonander.fernandezdo@elorrieta-errekamari.com>
2024-02-19 15:53:42 +0100

    web eginda ticketa

--- Web_zinema/sarrerakerosi/sarrerakerosi.php
+++ Web_zinema/sarrerakerosi/sarrerakerosi.php
@@ -8,8 +8,9 @@
     <link rel="stylesheet" href="sarrerakerosi.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <script>
-         function datuakegin(){
-            var idFilm = window.location.href.split("=")[1];
+        function datuakegin() {
+            var idFilm = (new URLSearchParams(window.location.search)).get('id_film');
+
             <?php
             $mysqli = new mysqli("localhost", "root", "", "db_ElorrietazinemaT5");
             if ($mysqli->connect_errno) {
@@ -23,19 +24,14 @@
             $emaitza = $mysqli->query($sql);
 
             while ($row = $emaitza->fetch_assoc()) {
-                ?>
+            ?>
 
-                var option = document.createElement("option");
-                option.text = '<?php echo $row['izena'] ?>';
-                option.value = '<?php echo $row['filma_id'] ?>';
-                document.getElementById("id_film").appendChild(option);
-                <?php
-
-
-                // echo "<option value='" . $row['filma_id'] . "'>" . $row['izena'] ."</option>";
+            var option = document.createElement("option");
+            option.text = '<?php echo $row['izena'] ?>';
+            option.value = '<?php echo $row['filma_id'] ?>';
+            document.getElementById("id_film").appendChild(option);
+            <?php
             }  
-
-
 
             $filma = $_GET["id_film"];
             $selectedZinema = isset($_GET["zinema_id"]) ? $_GET["zinema_id"] : ''; 
@@ -44,34 +40,31 @@
             $result = $mysqli->query($sql);
 
             while ($row = $result->fetch_assoc()) {
-                ?>
+            ?>
 
-                var option = document.createElement("option");
-                option.text = '<?php echo $row['izena'] ?>';
-                option.value = '<?php echo $row['zinema_id'] ?>';
-                document.getElementById("zinema").appendChild(option);
-                <?php
-
-
-                // echo "<option value='" . $row['zinema_id'] . "'>" . $row['izena'] . "</option>";
-
-                }
+            var option = document.createElement("option");
+            option.text = '<?php echo $row['izena'] ?>';
+            option.value = '<?php echo $row['zinema_id'] ?>';
+            document.getElementById("zinema").appendChild(option);
+            <?php
+            }
 
             $result->free();
 
-       
-        
-        if(isset($_GET["zinema"])){
+            if(isset($_GET["zinema"])){
             ?>
-            document.getElementById("zinema").value = <?php echo $_GET["zinema"] ?>;
-        <?php    
+            document.getElementById("zinema").value = '<?php echo $_GET["zinema"] ?>';
+            <?php    
+            }
+            ?>
+
+            var urlParams = new URLSearchParams(window.location.search);
+            var fechaParam = urlParams.get('data');
+            if (fechaParam) {
+                document.getElementById("data").value = fechaParam;
+            }
         }
-        ?>
 
-
-    }
-        
-    
         function Prezioakalkulatu() {
             var kant = document.getElementById("kant").value;
             var prezioa = parseFloat(kant) * 9.50; 
@@ -79,60 +72,53 @@
         }
 
         function setMinDate() {
-        var today = new Date();
-        var dd = String(today.getDate()).padStart(2, '0');
-        var mm = String(today.getMonth() + 1).padStart(2, '0');
-        var yyyy = today.getFullYear();
+            var today = new Date();
+            var dd = String(today.getDate()).padStart(2, '0');
+            var mm = String(today.getMonth() + 1).padStart(2, '0');
+            var yyyy = today.getFullYear();
 
-        today = yyyy + '-' + mm + '-' + dd;
-        document.getElementById("data").min = today;
-    }
+            today = yyyy + '-' + mm + '-' + dd;
+            document.getElementById("data").min = today;
+        }
+
         function zineAukeratu(){
-        var url = window.location.href.split("&")[0];
-        var zine = document.getElementById("zinema").value;
-        window.location.href = (url+ "&zinema=" + zine);
+            var url = window.location.href.split("&")[0];
+            var zine = document.getElementById("zinema").value;
+            window.location.href = (url+ "&zinema=" + zine);
+        }
 
-    }
         function dataAukeratu(){
-            var url = window.location.href.split("?")[0] ;
-        var data = document.getElementById("data").value;
-        var zinema = document.getElementById("zinema").value;
-        var filma = document.getElementById("id_film").value
-        window.location.href = (url+ "?id_film=" + filma + "&zinema=" + zinema + "&data=" + data);
-    }
+            var url = window.location.href.split("?")[0];
+            var data = document.getElementById("data").value;
+            var zinema = document.getElementById("zinema").value;
+            var filma = document.getElementById("id_film").value;
+            window.location.href = (url + "?id_film=" + filma + "&zinema=" + zinema + "&data=" + data);
+        }
 
-    function erosi() {
-        window.location.href = '../logina/logina.php'
+        function erosi() {
+            window.location.href = '../logina/logina.php'
         };
 
-        window.onload = setMinDate;
-
+        window.onload = function() {
+            setMinDate();
+            datuakegin();
+        };
     </script>
 
 </head>
-<body onload="datuakegin()">
+<body>
     <a href="../index.html" id="atzera"><img src="../img/fletxa.png" alt="fletxa"></a>
     <form method="get" id="sarrerakerosi" name="sarrerakerosi">
         <h1 class="title">Sarrerak</h1>
         <label for="pelikula">Pelikula:</label>
-    <select id= "id_film">
-
-            <?php
-           
-            
-            ?>
-            </select>
+        <select id="id_film"></select>
         <label for="zinema">
-        <i class="fa-solid fa-film"></i>
-        <select id="zinema" name="zinema_id" onchange="zineAukeratu()">
-            <?php
-            
-            ?>
-        </select>
-    </label>
+            <i class="fa-solid fa-film"></i>
+            <select id="zinema" name="zinema_id" onchange="zineAukeratu()"></select>
+        </label>
         <label for="data">
             <i class="fa-solid fa-calendar"></i>
-            <input type="date" id="data" onchange = "dataAukeratu()">
+            <input type="date" id="data" onchange="dataAukeratu()">
         </label>
         <label for="saioak">
             <i class="fa-solid fa-clock"></i>
@@ -154,29 +140,16 @@
                 <option></option>
 
             </select>
-        </label>
+            
         <label for="kant">
-    <i class="fa-solid fa-ticket"></i>
-    <input type="number" id="kant" value="1" min="1" onchange="Prezioakalkulatu()">
+            <i class="fa-solid fa-ticket"></i>
+            <input type="number" id="kant" value="1" min="1" onchange="Prezioakalkulatu()">
         </label>
-
         <label for="prezioa">
             <i class="fa-solid fa-money-bill"></i>
-            <?php
-            $sql = "SELECT prezioa FROM FILMA WHERE filma_id = $filma";
-            $result = $mysqli->query($sql);
-            
-            while ($row = $result->fetch_assoc()) {
-                echo "<input id='prezioa' onchange='Prezioakalkulatu()' readonly type='text' value=" . $row['prezioa'] . "> ";
-            }  
-
-            $mysqli->close();
-            ?>
+            <input id='prezioa' onchange='Prezioakalkulatu()' readonly type='text'>
         </label>
-
-        </label>
-        <input type="button" id="sarrerakerosiButton" value ="Sarrerak erosi" onclick ="erosi()">
+        <input type="button" id="sarrerakerosiButton" value="Sarrerak erosi" onclick="erosi()">
     </form>
-    
 </body>
 </html>
