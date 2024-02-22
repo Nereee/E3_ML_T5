4db41449c8ca972aec222cac185f6ee65129fead
ibai-elorrieta <157578123+ibai-elorrieta@users.noreply.github.com>
2024-02-22 11:55:49 +0100

    web amaituta

--- Web_zinema/sarrerakerosi/sarrerakerosi.php
+++ Web_zinema/sarrerakerosi/sarrerakerosi.php
@@ -1,3 +1,6 @@
+<?php
+session_start();
+?>
 <!DOCTYPE html>
 <html lang="eu">
 <head>
@@ -6,15 +9,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sarrerak</title>
     <link rel="stylesheet" href="sarrerakerosi.css">
-</head>
-<body onload="datuakegin()">
-    <a href="../index.html" id="atzera"><img src="../img/fletxa.png" alt="fletxa"></a>
-    <form action="sarrerakerosi.php" method="get" id="sarrerakerosi" name="sarrerakerosi">
-        <h1 class="title">Sarrerak</h1>
-        <label for="pelikula">
-            Pelikula: 
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script>
+        function datuakegin() {
+            var idFilm = (new URLSearchParams(window.location.search)).get('id_film');
+
             <?php
-            $mysqli = new mysqli("localhost", "root", "", "db_Elorrietazinema");
+            $mysqli = new mysqli("localhost", "root", "", "db_ElorrietazinemaT5");
             if ($mysqli->connect_errno) {
                 echo "Fallo al conectar a MySQL: " . $mysqli->connect_error;
                 exit();
@@ -22,98 +23,52 @@
 
             $idfilm = $_GET['id_film'];
 
-            $sql = "SELECT izena FROM FILMA WHERE filma_id = ?";
-            $stmt = $mysqli->prepare($sql);
+            $sql = "SELECT izena, filma_id FROM FILMA WHERE filma_id = $idfilm ";
+            $emaitza = $mysqli->query($sql);
 
-            if (!$stmt) {
-                echo "Error al preparar la consulta: (" . $mysqli->errno . ") " . $mysqli->error;
-                exit();
-            }
-            $stmt->bind_param("i", $idfilm);
-            if (!$stmt->execute()) {
-                echo "Error al ejecutar la consulta: (" . $stmt->errno . ") " . $stmt->error;
-                exit();
-            }
-            $stmt->bind_result($pelikula_izena);
-            $stmt->fetch();
-            echo "<input readonly type='text' value='" . $pelikula_izena . "' >";
-            $stmt->close();
-            $mysqli->close();
+            while ($row = $emaitza->fetch_assoc()) {
+                $_SESSION ['filma_izena'] = $row['izena'] ;
             ?>
-        </label>
-        <label for="zinemak">
-            <i class="fa-solid fa-film"></i>
-            <select id="zinemak">
-                <?php
-                $mysqli = new mysqli("localhost", "root", "", "db_Elorrietazinema");
-                if ($mysqli->connect_errno) {
-                    echo "Fallo al conectar a MySQL: " . $mysqli->connect_error;
-                    exit();
-                }
-                $sql = "SELECT zinema_id, izena FROM ZINEMA";
-                $result = $mysqli->query($sql);
-                while ($row = $result->fetch_assoc()) {
-                    echo "<option value='" . $row['zinema_id'] . "'>" . $row['izena'] . "</option>";
-                }               
-                $result->free();
-                $mysqli->close();
-                ?>
-            </select>
-        </label>
-        <label for="data">
-            <i class="fa-solid fa-calendar"></i>
-            <input type="date" id="data">
-        </label>
-        <label for="saioak">
-            <i class="fa-solid fa-clock"></i>
-            <select id="saioak">
-                <option value="12:20">12:20</option>
-                <option value="17:30">17:30</option>
-                <option value="20:00">20:00</option>
-            </select>
-        </label>
-        <label for="kant">
-            <i class="fa-solid fa-ticket"></i>
-            <input type="number" id="kant" value="1" min="1" onchange="Prezioakalkulatu()">
-        </label>
-        <label for="prezioa">
-    <i class="fa-solid fa-money-bill"></i>
-    <?php
-    $mysqli = new mysqli("localhost", "root", "", "db_Elorrietazinema");
-    if ($mysqli->connect_errno) {
-        echo "Fallo al conectar a MySQL: " . $mysqli->connect_error;
-        exit();
-    }
-    $id_film = 1;
-    $sql = "SELECT prezioa FROM FILMA WHERE filma_id = ?";
-    $stmt = $mysqli->prepare($sql);
-    if (!$stmt) {
-        echo "Error al preparar la consulta: (" . $mysqli->errno . ") " . $mysqli->error;
-        exit();
-    }
-    $stmt->bind_param("i", $id_film);
-    if (!$stmt->execute()) {
-        echo "Error al ejecutar la consulta: (" . $stmt->errno . ") " . $stmt->error;
-        exit();
-    }
-    $stmt->bind_result($prezioa);
-    $stmt->fetch();
-    echo "<input type='number' id='prezioa' value='$prezioa' readonly>";
-    $stmt->close();
-    $mysqli->close();
-    ?>
-</label>
-        <button id="sarrerakerosiButton">Sarrerak erosi</button>
-    </form>
 
-    <script>
-        document.getElementById("sarrerakerosiButton").onclick = function (e) {
-        e.preventDefault(); 
-        
-        window.location.href = '../logina/logina.php'
-        };
-        function datuakegin(){
-            var idFilm = window.location.href.split("=")[1];
+            var option = document.createElement("option");
+            option.text = '<?php echo $row['izena'] ?>';
+            option.value = '<?php echo $row['filma_id'] ?>';
+            document.getElementById("id_film").appendChild(option);
+            <?php
+            }  
+
+            $filma = $_GET["id_film"];
+            $selectedZinema = isset($_GET["zinema_id"]) ? $_GET["zinema_id"] : ''; 
+           
+            $sql = "SELECT DISTINCT zinema_id, izena FROM ZINEMA INNER JOIN SAIOA USING (zinema_id) WHERE filma_id = $filma";
+
+            $result = $mysqli->query($sql);
+
+            while ($row = $result->fetch_assoc()) {
+                $_SESSION ['zinema_izena'] = $row['izena'] ;
+            ?>
+
+            var option = document.createElement("option");
+            option.text = '<?php echo $row['izena'] ?>';
+            option.value = '<?php echo $row['zinema_id'] ?>';
+            document.getElementById("zinema").appendChild(option);
+            <?php
+            }
+
+            $result->free();
+
+            if(isset($_GET["zinema"])){
+            ?>
+            document.getElementById("zinema").value = '<?php echo $_GET["zinema"] ?>';
+            <?php    
+            }
+            ?>
+
+            var urlParams = new URLSearchParams(window.location.search);
+            var fechaParam = urlParams.get('data');
+            if (fechaParam) {
+                document.getElementById("data").value = fechaParam;
+            }
         }
 
         function Prezioakalkulatu() {
@@ -123,16 +78,121 @@
         }
 
         function setMinDate() {
-        var today = new Date();
-        var dd = String(today.getDate()).padStart(2, '0');
-        var mm = String(today.getMonth() + 1).padStart(2, '0');
-        var yyyy = today.getFullYear();
+            var today = new Date();
+            var dd = String(today.getDate()).padStart(2, '0');
+            var mm = String(today.getMonth() + 1).padStart(2, '0');
+            var yyyy = today.getFullYear();
 
-        today = yyyy + '-' + mm + '-' + dd;
-        document.getElementById("data").min = today;
-    }
+            today = yyyy + '-' + mm + '-' + dd;
+            document.getElementById("data").min = today;
+        }
 
-    window.onload = setMinDate;
+        function zineAukeratu(){
+            var url = window.location.href.split("&")[0];
+            var zine = document.getElementById("zinema").value;
+            window.location.href = (url+ "&zinema=" + zine);
+        }
+
+        function dataAukeratu(){
+            var url = window.location.href.split("?")[0];
+            var data = document.getElementById("data").value;
+            var zinema = document.getElementById("zinema").value;
+            var filma = document.getElementById("id_film").value;
+            var kant = document.getElementById("kant").value; 
+            
+            window.location.href = (url + "?id_film=" + filma + "&zinema=" + zinema + "&data=" + data + "&kant=" + kant);
+
+            <?php
+            $_SESSION['id_film'] = $_GET['id_film']; 
+
+            if(isset($_GET["zinema"])&& isset($_GET['data'])&& isset($_GET['saioak'])&& isset($_GET['kant'])&& isset($_GET['prezioa']) ) {
+            $_SESSION['id_zinema'] = $_GET["zinema"];  
+            $_SESSION['id_data'] = $_GET['data'];   
+            $_SESSION['id_saioa'] = $_GET['saioak'];  
+            $_SESSION['id_kant'] = $_GET['kant'];  
+            $_SESSION['id_prezioa'] = $_GET['prezioa'];   
+            }
+           
+            ?>
+        }
+
+        function Prezioakalkulatu() {
+            var url = window.location.href;
+            
+
+            var kant = document.getElementById("kant").value;
+            var prezioa = parseFloat(kant) * 9.50; 
+            document.getElementById("prezioa").value = prezioa.toFixed(2); 
+        }
+      
+        function erosi() {
+    var idFilm = document.getElementById("id_film").value;
+    var idZinema = document.getElementById("zinema").value;
+    var data = document.getElementById("data").value;
+    var kant = document.getElementById("kant").value; 
+    var prezioa = document.getElementById("prezioa").value; 
+    
+    
+    var url = '../logina/logina.php?' + 'id_film=' + idFilm + '&zinema=' + idZinema + '&data=' + data + '&kant=' + kant + '&prezioa=' + prezioa; 
+    
+    window.location.href = url;
+}
+
+
+        window.onload = function() {
+            setMinDate();
+            datuakegin();
+        };
     </script>
+
+</head>
+<body>
+    <a href="../index.html" id="atzera"><img src="../img/fletxa.png" alt="fletxa"></a>
+    <form method="get" id="sarrerakerosi" name="sarrerakerosi">
+        <h1 class="title">Sarrerak</h1>
+        <select id="id_film"></select><br>
+        <label for="zinema">
+            <i class="fa-solid fa-film"></i>
+            <select id="zinema" name="zinema_id" onchange="zineAukeratu()"></select>
+        </label>
+        <label for="data">
+            <i class="fa-solid fa-calendar"></i>
+            <input type="date" id="data" onchange="dataAukeratu()">
+        </label>
+        <label for="saioak">
+            <i class="fa-solid fa-clock"></i>
+            <select name = "saioak" id="saioak">
+            <?php
+                $data = $_GET["data"];
+                $zinema = $_GET["zinema"];
+
+                $sql = "SELECT saioa_id, ordutegia, f.izena, eguna FROM saioa s INNER JOIN filma f USING(filma_id) WHERE s.filma_id = $filma AND eguna = '$data' AND s.zinema_id = '$zinema'";
+
+                $result = $mysqli->query($sql);
+
+                while ($row = $result->fetch_assoc()) {
+                    echo "<option value='" . $row['eguna'] . "'>" . $row['ordutegia'] . "</option>";
+                    $_SESSION ['data'] = $row['eguna'] ;
+                    $_SESSION ['ordua'] = $row['ordutegia'] ;
+                    $_SESSION ['id_saioa'] = $row['saioa_id'] ;
+                }
+
+                $result->free();
+                ?>
+
+                <option></option>
+
+            </select>
+            <br><br>
+        <label for="kant">
+            <i class="fa-solid fa-ticket"></i>
+            <input type="number" id="kant" name = "kant" min="1" onchange="Prezioakalkulatu()">
+        </label> <br><br>
+        <label for="prezioa">
+            <i class="fa-solid fa-money-bill"></i>
+            <input id='prezioa' name = "prezioa" onchange='Prezioakalkulatu()' readonly type='text'>
+        </label><br><br>
+        <button type="button" id="sarrerakerosiButton" onclick="erosi()" style="color: #ffc65d; border: none; background: linear-gradient(to right, #000000, #000000); padding: 10px 15px; cursor: pointer; font-size: 20px;">Sarrerak erosi</button>
+    </form>
 </body>
 </html>
